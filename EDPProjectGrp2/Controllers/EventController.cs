1fd29edbf89ff6a3544454c58cdc97548193a5b0
sam <samuelongqixin@outlook.com>
2024-01-09 12:24:32 +0800

    creation of events model

---
+++ EDPProjectGrp2/Controllers/EventController.cs
@@ -0,0 +1,95 @@
+﻿using LearningAPI;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EDPProjectGrp2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventController : Controller
+    {
+        private readonly MyDbContext _context;
+        private readonly IConfiguration _configuration;
+
+        public EventController(MyDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        // GET: EventController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: EventController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EventController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EventController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EventController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EventController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EventController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EventController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ EDPProjectGrp2/Models/Event.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EDPProjectGrp2.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string EventName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(1000)]
+        public string EventDescription { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string EventCategory { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(255)]
+        public string EventLocation { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, 999)]
+        public int EventTicketStock { get; set; } = 0;
+
+        [StringLength(500)]
+        public string EventPicture { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0, 999)]
+        public int EventPrice { get; set; } = 0;
+
+        [Range(0, 999)]
+        public int EventUplayMemberPrice { get; set; } = 0;
+
+        [Range(0, 999)]
+        public int EventNtucClubPrice { get; set; } = 0; 
+
+        [Required]
+        public DateTime EventDate { get; set; } = DateTime.MinValue;
+
+        [Required]
+        [Range(0, 999)]
+        public int EventDuration { get; set; } = 0;
+
+        [Required]
+        public bool EventSale { get; set; } = false;
+
+        [Required]
+        public bool EventStatus { get; set; } = false;
+    }
+}
