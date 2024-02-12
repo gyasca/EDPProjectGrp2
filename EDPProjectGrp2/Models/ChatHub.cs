c5e4aa7593532b8d7d1df73302ca4ced48fa36d7
azrelazyzz <iamazrel@gmail.com>
2024-02-12 10:28:01 +0800

    chathub backend

--- EDPProjectGrp2/Models/ChatHub.cs
+++ EDPProjectGrp2/Models/ChatHub.cs
@@ -2,13 +2,13 @@
 
 public class ChatHub : Hub
 {
-    public async Task SendMessage(string message)
+    public async Task SendMessage(string message, string user)
     {
         try
         {
             // Your existing logic for processing the message
 
-            await Clients.All.SendAsync("ReceiveMessage", message);
+            await Clients.All.SendAsync("ReceiveMessage", message, user);
         }
         catch (Exception ex)
         {
@@ -16,5 +16,10 @@ public class ChatHub : Hub
             // You can also send an error message back to the client if needed
             await Clients.Caller.SendAsync("ReceiveErrorMessage", ex.Message);
         }
+    }
+
+    public async Task ResolveTicket(string user)
+    {
+        await Clients.User(user).SendAsync("Ticket Resolved");
     }
 }
